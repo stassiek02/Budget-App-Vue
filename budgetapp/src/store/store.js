06e1caa369a4646c723f13ff2111f892bbca75ce
Daniel <staszekdaniel5@gmail.com>
2019-11-26 19:44:56 +0100

    add vuex persist

--- budgetapp/src/store/store.js
+++ budgetapp/src/store/store.js
@@ -1,8 +1,13 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import VuexPersist from "vuex-persist";
 
 Vue.use(Vuex);
 
+const vuexLocalStorage = new VuexPersist({
+  key: "vuex",
+  storage: window.localStorage
+});
 export const store = new Vuex.Store({
   state: {
     incList: [],
@@ -31,5 +36,6 @@ export const store = new Vuex.Store({
         return acc + parseFloat(item.value);
       }, 0);
     }
-  }
+  },
+  plugins: [vuexLocalStorage.plugin]
 });
