f5c31786b7f91e906bef8e100b17b856f7850a34
Daniel <staszekdaniel5@gmail.com>
2019-11-26 11:20:59 +0100

    Add vuex persist

--- budgetapp/src/store/store.js
+++ budgetapp/src/store/store.js
@@ -1,24 +1,33 @@
 import Vue from "vue";
 import Vuex from "vuex";
-
+import VuexPersist from "vuex-persist";
 Vue.use(Vuex);
+
+const vuexLocalStorage = new VuexPersist({
+  key: "vuex",
+  storage: window.localStorage
+});
 
 export const store = new Vuex.Store({
   state: {
     incList: [],
-    expList:[],
+    expList: []
   },
   mutations: {
     change(state, payload) {
       state[payload.property].push(payload.with);
     },
-    deleteItem(state,payload){
-      console.log(payload.with)
-      state[payload.property].splice(state[payload.property].indexOf(payload.with),1);
+    deleteItem(state, payload) {
+      console.log(payload.with);
+      state[payload.property].splice(
+        state[payload.property].indexOf(payload.with),
+        1
+      );
     }
   },
-  getters:{
-      getIncList:state =>state.incList,
-      getExpList:state =>state.expList
-  }
+  getters: {
+    getIncList: state => state.incList,
+    getExpList: state => state.expList
+  },
+  plugins: [vuexLocalStorage.plugin]
 });
